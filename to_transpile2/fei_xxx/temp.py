1473f7eed23d16c11e96fc23da79ad14be754bf6
Dave Karetnyk <Dave.Karetnyk@gmail.com>
2021-08-03 12:42:35 +0200

    example AutoStar setup.py setup

---
+++ to_transpile2/fei_xxx/temp.py
@@ -0,0 +1,153 @@
+from pprint import pprint
+import glob
+from pathlib import Path
+from pprint import pprint
+from setuptools import Extension
+import os
+
+if __name__ == '__main__':
+    target_dir = Path(__file__).parent / "to_transpile"
+
+    target_files = [str(path.absolute()) for path in Path(target_dir).glob("*.pyx")]
+    print(f"target pyx files to process from dir: {target_dir}")
+    pprint(target_files)
+
+    cython_extension_directories = []
+    for dirpath, directories, files in os.walk(target_dir):
+        # if the directory contains pyx files, cythonize it
+        if len(glob.glob('{0}/*.pyx'.format(dirpath))) > 0:
+            cython_extension_directories.append(dirpath)
+    print("************")
+    pprint(cython_extension_directories)
+    print("************: dirpath")
+    print(dirpath)
+    print(['{}/*.pyx'.format(dirpath)])
+    print("************: directories")
+    print(directories)
+    print("************: files")
+    print(files)
+
+    my_list = [
+        Extension(
+            '*',
+            ['{}/*.pyx'.format(dirpath)],
+            libraries=['ole32', 'oleaut32', 'advapi32'],
+
+            # Leave optimization as is ('Ox' apparently) but generate full debug info.
+            extra_compile_args=['-Zi'],
+
+            # /IGNORE:4197: suppress warning of function declared for export more than once
+            # -debug=full: use debug info to create pdb files
+            extra_link_args=['/IGNORE:4197', '-debug:full'],
+
+        )
+        for dirpath in cython_extension_directories
+    ]
+
+    target_files = [str(path.absolute()).replace(".pyx", ".pdb")
+                    for path in Path(target_dir).rglob("*.pyx")]
+    print(f"target pyx files to process from dir: {target_dir}")
+    pprint(target_files)
+
+    target_files = [str(path.absolute()).
+                    replace(str(target_dir), "").
+                    replace(".pyx", "").
+                    replace("\\", ".")
+                    for path in Path(target_dir).rglob("*.pyx")]
+    # print(f"target pyx files to process from dir: {target_dir}")
+    # pprint(target_files)
+
+    # os.system('set')
+
+    from multiprocessing import cpu_count
+    # This seems to be the number of logical CPUs.
+    print(f"multiprocessing module says cpu count is: {cpu_count()}")
+
+    # target_dir = Path(__file__).parent / "to_transpile"
+    # file_names = [str(file_name.resolve()) for file_name in Path(target_dir).rglob("*.pyx")]
+    # pprint(file_names)
+    # print(len(file_names))
+
+    target = "C:\\a\\b\\c\\hello.pyx"
+    print(target)
+    pdb_name = f"-Fd{target.replace('.pyx', '.pdb')}"
+    print(target)
+    print(pdb_name)
+
+    # Don't need to calculate module name if Extension calls is passed "*" for name.
+    dist_name = "fei_some_comp"
+    #  _dir = "C:\\work_dir\\fei_some_comp"
+    file_name = f"C:\\work_dir\\fei_some_comp\\a\\b\\c\\hello.pyx"
+    mod_file_name = file_name.replace(file_name.split(dist_name)[0], "")
+    # mod_file_name = file_name[file_name.find(dist_name):]
+    mod_name = mod_file_name.replace(".pyx", "").replace("\\", ".")
+    # mod_name = mod_name[1:]  # get rid of leading . in name
+    # print(f"_dir:               {_dir}")
+    print(f"file_name:              {file_name}")
+    print(f"module file name:       {mod_file_name}")
+    print(f"module dotted name:     {mod_name}")
+
+    x = file_name.split(dist_name)
+    print(x)
+
+    import pathlib
+    import shutil
+    from glob import glob
+
+    # src_file = glob(
+    #     r'C:\github\cython_testing\to_transpile2\my_utils\build\lib.win-amd64-3.6\fei_xxx\*.pdb')
+    # print(f"found: {src_file}")
+    # src_file = src_file[0]
+    #
+    # tmp_subdir = r"\build\lib.win-amd64-3.6"
+    #
+    # dst_file = src_file.replace(tmp_subdir, "")
+
+    # my_file = pathlib.Path(my_file[0])
+    # to_file = pathlib.Path(r'C:\github\cython_testing\to_transpile2\my_utils\fei_xxx')
+    #
+    # shutil.copy(src_file, dst_file)
+
+    # some_dir = pathlib.Path(r'C:\github\cython_testing\to_transpile2\my_utils\fei_xxx')
+    # parent_dir = some_dir.parent
+    # int_dir = some_dir.parent / "build/lib.win-amd64-3.6"
+    # print(f"some dir: {some_dir}")
+    # print(f"int dir:  {int_dir}")
+
+    # some_dir = pathlib.Path(r'C:\github\cython_testing\to_transpile2\my_utils\fei_xxx')
+    # _files = [_file for _file in Path(some_dir).rglob("*.pdb")
+    #           if not _file.match("*win_amd64.pdb")]
+    # print("found files...")
+    # for _file in _files:
+    #     print(f"    found: {_file}")
+
+    # from tfs_cythonize import _tidy_up_pdb_files
+    # _tidy_up_pdb_files(r'C:\github\cython_testing\to_transpile2\my_utils\fei_xxx')
+
+    from tfs_cythonize import find_dist_base
+    some_dir = r'C:\github\cython_testing\to_transpile2\my_utils\fei_xxx\\'
+    base_dir, package_path = find_dist_base(some_dir)
+    print(f"checking: {some_dir}")
+    print(f"    base_dir:       {base_dir}")
+    print(f"    package_path:   {package_path}")
+
+    some_dir = r'C:\github\cython_testing\to_transpile\fei_xxx'
+    base_dir, package_path = find_dist_base(some_dir)
+    print(f"checking: {some_dir}")
+    print(f"    base_dir:       {base_dir}")
+    print(f"    package_path:   {package_path}")
+
+    from Cython.Build.Dependencies import extended_iglob
+    some_dir = r'C:\github\cython_testing\to_transpile2\my_utils\fei_xxx'
+    all_paths = map(os.path.abspath, extended_iglob(some_dir))
+    for path in all_paths:
+        print(f"entry: {path}")
+        print(f"type:  {type(path)}")
+
+    import os
+    import multiprocessing
+    print(f"cpu count from multiprocessing packages: {multiprocessing.cpu_count()}")
+    print(f"cpu count from os:                       {os.cpu_count()}")
+
+    this_file_name = Path(__file__).stem
+    print(f"{this_file_name}")
---
+++ to_transpile2/setup.py
@@ -0,0 +1,175 @@
+# Copyright (c) 2016-2021 by FEI Company
+# All rights reserved. This file includes confidential and proprietary
+# information of FEI Company.
+""" Example 'AutoStar style' setup.py.
+"""
+from os import path
+import sys
+from pathlib import Path
+from setuptools import setup, find_packages
+from setuptools.command.develop import develop
+
+from Cython.Distutils import build_ext
+
+# Add parent directory of this module to path: needed utility code lives outside the normal
+# source tree.
+sys.path.append(
+    path.abspath(path.join(path.dirname(__file__), path.pardir)))
+from setup_utilities import cythonize  # noqa 402
+
+# Packages to cythonize protect company sensitive material.
+CYTHON_INCLUDE_PACKAGES = [
+    r'fei_common\infra',
+    r'fei_common\applications\sherpa',
+]
+
+# Cython exclusions (package and modules) e.g.:
+# * Issue at runtime when using RawImage: '_get_data() takes exactly one argument (0 given)'
+#   Maybe it's due to the fact that C does not support defaults?
+# * UI code excluded - issues at least with QThread class and signal handlers.
+# * Simulate utilities code: walks the Python call stack so does not work as complied C code.
+
+# Packages (directories) that should not be processed.
+CYTHON_EXCLUDED_PACKAGES = [
+    r'fei_common\applications\sherpa\ui\gen',
+    r'fei_common\infra\tem_service\imaging',
+]
+
+# Modules (files) that should not be processed.
+CYTHON_EXCLUDED_MODULES = [
+    r'start_',
+    r'signal_handler',
+    r'main_window',
+    r'generic_procedure',
+    r'utilities',
+    r'configure_cmd_files',  # excluded due to use of __file__ at module scope
+    r'conftest.py',
+]
+
+# Items (directories and files) to exclude from the cythonized distributions.
+DIST_EXCLUDED_PACKAGES = [
+    '*.tests',
+    '*.tests.*',
+    'tests.*',
+    'tests',
+    'dev_*',
+    r'adjust_build_info.*',
+    r'Build-SherpaUiSource.*',
+    # r'fei\autostar\applications\sherpa\plugins\dummy_plugin'
+    r'conftest.py',
+]
+
+EXCLUDE_FOR_TEST = [
+    'dev_*',
+    r'adjust_build_info.*',
+    r'Build-SherpaUiSource.*',
+]
+
+DIST_EXCLUDED_ITEMS_FROM_TEST_WHL = [
+    r'tests',
+    r'conftest.py',
+]
+
+_here = Path(__file__).resolve().parents[0]
+
+
+class DevelopCommand(develop):
+    """ Setuptools command to configure development setup.
+
+    Sub-class of standard 'develop' command to allow additional custom code
+    to be executed.
+    """
+
+    # Inheriting from old style Python class so no __init__ and super call.
+    # def __init__(self):
+    #     super(DevelopCommand, self).__init__()
+
+    description = 'FEI AutoStar_Common wheel: "development mode".'
+
+    def run(self):
+        develop.run(self)
+        if not self.uninstall:
+            alt_script = '../python/utilities/Set-ShortcutsToCurrentPythonEnv.ps1'
+            print("setup.py: NOTE: auto configure cmd/ps1 shortcuts disabled!")
+            print(f"\t\tinstead use {alt_script}")
+
+    def initialize_options(self):
+        develop.initialize_options(self)
+
+    def finalize_options(self):
+        develop.finalize_options(self)
+
+
+class CythonizeCommand(build_ext):
+    """ Setuptools command to cythonize the application (without
+    test directories, test scripts, and test applications).
+
+    It converts tagged py files to Cython extensions and builds them.
+    """
+
+    # Inheriting from old style Python class so no __init__ and super call.
+    # def __init__(self):
+    #     super(DevelopCommand, self).__init__()
+
+    description = 'FEI AutoStar_Common wheel: fully cythonized'
+
+    def initialize_options(self):
+        build_ext.initialize_options(self)
+
+    def run(self):
+        cythonize.build_cython_packages(self,
+                                        root=_here,
+                                        wheel_name="fei_xxx",
+                                        dist_root_name="fei_xxx",
+                                        dist_excluded_packages=DIST_EXCLUDED_PACKAGES,
+                                        cython_include_packages=CYTHON_INCLUDE_PACKAGES,
+                                        cython_excluded_packages=CYTHON_EXCLUDED_PACKAGES,
+                                        cython_excluded_modules=CYTHON_EXCLUDED_MODULES,
+                                        dist_excluded_items=DIST_EXCLUDED_ITEMS_FROM_TEST_WHL)
+
+
+# print(">>> ENTER SETUP.PY")  # ???DaveK temp
+# from pprint import pprint
+# pprint(globals())
+setup(
+    name="fei_xxx",
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version="1.2.3",
+
+    description="example dist - fei_xxx",
+    long_description=f"example dist - fei_xxx ... for test purposes",
+
+    # Install all packages (folders and files) in this folder.
+    # https://pythonhosted.org/setuptools/setuptools.html#including-data-files
+    packages=find_packages(where=".", exclude=["setup_utilities"]),
+
+    cmdclass={
+        "develop": DevelopCommand,
+        "cythonize": CythonizeCommand,
+    },
+
+    package_data={
+        # Files with these extensions in any package will be included.
+        #   .pyw: Python source that can be launched without a command shell popping up.
+        #   .pyd: typically a binary extension dll.
+        #   .dll: dll binary.
+        #   .cmd: Script file, e.g. for running a Python script.
+        #   .ps1: PowerShell file, e.g. for running a Python script.
+        #   .htm: used for help documentation.
+        #   .reg: RegEdit 5 file, e.g. used to set logging options for some dev tools.
+        #   .feature: BDD feature file, containing test script in Gherkin language.
+        #   .xml: XML file, used for configuration.
+        #   .config: XML configuration file, used for DSAL Data Services logging.
+        #   .csv: comma separated file used in tests.
+        #   .dat: file containing test data.
+        #   .emi: file containing test image(s).
+        '': ['*.pyw', '*.pyd', '*.dll', '*.cmd', '*.ps1', '*.htm', '*.reg', '*.feature', '*.xml',
+             '*.config', '*.csv', '*.dat', '*.emi'],
+    },
+)
+# print(">>> EXIT SETUP.PY")  # ???DaveK temp
+# from pprint import pprint
+# pprint(globals())
